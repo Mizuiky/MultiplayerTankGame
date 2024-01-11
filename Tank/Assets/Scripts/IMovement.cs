0601556ebd6a5fba16e91d775f0d995ed18b2f1b
Priscila da Rocha Alves <priscila.mizuiky@gmail.com>
2024-01-11 17:48:06 -0300

    Add movement classes

---
+++ Tank/Assets/Scripts/IMovement.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IMovement
+{
+    public void Move(Vector2 input);
+
+    public void Rotate();
+}
---
+++ Tank/Assets/Scripts/Movement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour, IMovement
+{
+    public void Move(Vector2 input)
+    {
+
+    }
+
+    public void Rotate()
+    {
+
+    }
+}
